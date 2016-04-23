6d7c08cff8e655ce120f6c43179740ff2c17b9ed
Walter Gray <wgray@leapmotion.com>
2016-04-22 20:16:43 -0700

    Fixed protobuf builds

--- script/protobuf.x64.cmd
+++ script/protobuf.x64.cmd
@@ -1,18 +1,9 @@
 @setlocal
 @set root_dir=%CD%
 @call %VSSETUP_COMMAND%
-@cd src
-set PROTOBUF_SRC=%~dpnx1
-set PROTOBUF_SRC=%PROTOBUF_SRC:\=/%
-cd protobuf-cmake
-call %CMAKE_COMMAND% -DPROTOBUF_ROOT="%PROTOBUF_SRC%" -G"Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=%2\%3
-call %CMAKE_COMMAND% --build . --config debug
-::@call msbuild /t:INSTALL FlatBuffers.sln
-::@nmake -f win32/Makefile.msc AS=ml64 LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj"
-::@mkdir %2\%3
-::@mkdir %2\%3\include
-::@mkdir %2\%3\lib
-::@copy zconf.h %2\%3\include
-::@copy zlib.h %2\%3\include
-::@copy zlib.lib %2\%3\lib
+@cd src\%1
+@cd cmake
+call %CMAKE_COMMAND% -Dprotobuf_BUILD_TESTS:bool=false -DPROTOBUF_ROOT="%PROTOBUF_SRC%" -G"Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=%2
+call %CMAKE_COMMAND% --build . --target install --config debug
+call %CMAKE_COMMAND% --build . --target install --config release
 @endlocal
