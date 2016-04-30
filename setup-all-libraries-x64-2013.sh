25d86b24059afdcfd41b3c7922e6fbabd75a2efa
Walter Gray <wgray@leapmotion.com>
2016-04-29 18:00:28 -0700

    Added 2013 build scripts

---
+++ setup-all-libraries-x64-2013.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export INSTALL_ROOT=/c/dev/Libraries-x64_vc12
+export BUILD_ARCH=x64
+export MSVC_VER=2013
+
+source setup-all-libraries.sh
---
+++ setup-all-libraries-x86-2013.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export INSTALL_ROOT=/c/dev/Libraries-x86_vc12
+export BUILD_ARCH=x86
+export MSVC_VER=2013
+
+source setup-all-libraries.sh
