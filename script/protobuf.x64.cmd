be2a63d241f585db97ec00fa8fc54eac33f1a2c0
Walter Gray <wgray@leapmotion.com>
2016-04-29 17:56:58 -0700

    Fix protobuf to not use the static runtime, updated it to read MSVC_VER and ARCH

--- script/protobuf.x64.cmd
+++ script/protobuf.x64.cmd
@@ -3,7 +3,19 @@
 @call %VSSETUP_COMMAND%
 @cd src\%1
 @cd cmake
-call %CMAKE_COMMAND% -Dprotobuf_BUILD_TESTS:bool=false -DPROTOBUF_ROOT="%PROTOBUF_SRC%" -G"Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=%2
+
+if "%MSVC_VER%"=="2015" (
+  set CMAKE_GEN=Visual Studio 14 2015
+)
+if "%MSVC_VER%"=="2013" (
+  set CMAKE_GEN=Visual Studio 12 2013
+)
+if "%ARCH%"=="64" {
+  set CMAKE_GEN=%CMAKE_GEN% Win64
+}
+
+call %CMAKE_COMMAND% -Dprotobuf_BUILD_TESTS:bool=false -Dprotobuf_MSVC_STATIC_RUNTIME:bool=false -DPROTOBUF_ROOT="%PROTOBUF_SRC%" -G"%CMAKE_GEN%" -DCMAKE_INSTALL_PREFIX=%2
 call %CMAKE_COMMAND% --build . --target install --config debug
 call %CMAKE_COMMAND% --build . --target install --config release
+
 @endlocal
