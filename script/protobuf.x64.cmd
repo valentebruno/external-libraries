6d88bea466029eb4196b5ec358e264a9637c66ac
Walter Gray <wgray@leapmotion.com>
2016-05-03 20:55:28 -0700

    added zlib support to protobuf

--- script/protobuf.x64.cmd
+++ script/protobuf.x64.cmd
@@ -14,7 +14,7 @@ if "%BUILD_ARCH%"=="x64" (
   set CMAKE_GEN=%CMAKE_GEN% Win64
 )
 
-call %CMAKE_COMMAND% -Dprotobuf_BUILD_TESTS:BOOL=false -Dprotobuf_MSVC_STATIC_RUNTIME:BOOL=false -G"%CMAKE_GEN%" -DCMAKE_INSTALL_PREFIX=%2
+call %CMAKE_COMMAND% -Dprotobuf_BUILD_TESTS:BOOL=false -Dprotobuf_MSVC_STATIC_RUNTIME:BOOL=false -Dprotobuf_WITH_ZLIB:BOOL=true -DZLIB_ROOT:PATH="%2/../zlib-1.2.8" -G"%CMAKE_GEN%" -DCMAKE_INSTALL_PREFIX=%2
 call %CMAKE_COMMAND% --build . --target install --config debug
 call %CMAKE_COMMAND% --build . --target install --config release
 
