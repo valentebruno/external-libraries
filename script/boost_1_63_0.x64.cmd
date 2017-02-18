dba18fa2849c8d77669cf682f48216b0c1b5544a
Walter Gray <wgray@leapmotion.com>
2017-02-18 00:27:19 -0800

    Fix boost build, updated to 1.63, prevent building unused libraries

---
+++ script/boost_1_63_0.x64.cmd
@@ -0,0 +1,26 @@
+@setlocal
+@set root_dir=%CD%
+@cd src\%1
+@call %VSSETUP_COMMAND%
+
+if "%MSVC_VER%"=="2015" (
+  set TOOLSET=msvc-14.0
+)
+
+if "%MSVC_VER%"=="2013" (
+  set TOOLSET=msvc-12.0
+)
+
+set BUILD_DIR=build
+if "%BUILD_ARCH%"=="x64" (
+  set BUILD_DIR=%BUILD_DIR%64
+  set MEM_OPT=address-model=64
+  set BUILD_ARCH="ia64"
+)
+
+@call bootstrap.bat
+@b2 toolset=%TOOLSET% --with-atomic --with-chrono --with-date_time --with-filesystem --with-locale --with-program_options --with-thread ^
+ -j8 --build-dir=%root_dir%\%BUILD_DIR%\%1 --prefix=%2 ^
+  variant=debug variant=release link=static runtime-link=shared architecture=%BUILD_ARCH% %MEM_OPT% install
+
+@endlocal
