678a18100d3039a92f669b7e0abfa0b06d0d122f
Walter Gray <wgray@leapmotion.com>
2017-02-20 14:20:54 -0800

    Properly fixed boost_1_63_0.x64 working when multiple versions of VS are installed

--- script/boost_1_63_0.x64.cmd
+++ script/boost_1_63_0.x64.cmd
@@ -5,10 +5,13 @@
 
 if "%MSVC_VER%"=="2015" (
   set TOOLSET=msvc-14.0
+  set TOOLSET_VER=14.0
 )
 
 if "%MSVC_VER%"=="2013" (
   set TOOLSET=msvc-12.0
+  set TOOLSET_VER=12.0
+  set VS140COMNTOOLS=
 )
 
 set BUILD_DIR=build
@@ -19,8 +22,16 @@ if "%BUILD_ARCH%"=="x64" (
 )
 
 @call bootstrap.bat
-@b2 toolset=%TOOLSET% --with-atomic --with-chrono --with-date_time --with-filesystem --with-locale --with-program_options --with-thread ^
- -j8 --build-dir=%root_dir%\%BUILD_DIR%\%1 --prefix=%2 ^
- variant=debug variant=release link=static runtime-link=shared architecture=%BUILD_ARCH% %MEM_OPT% install
+SETLOCAL=ENABLEDELAYEDEXPANSION
+for /f "eol=; tokens=*" %%a in (project-config.jam) do (
+  set foo=%%a
+  if "!foo!"=="using msvc ; " set "foo=using msvc : %TOOLSET_VER% ; "
+  @echo(!foo! >> project-config-fixed.jam
+  )
+move project-config-fixed.jam project-config.jam
+
+@b2 --ignore-config --with-atomic --with-chrono --with-date_time --with-filesystem --with-locale --with-program_options --with-thread ^
+  -j8 --build-dir=%root_dir%\%BUILD_DIR%\%1 --prefix=%2  ^
+  variant=debug variant=release link=static runtime-link=shared architecture=%BUILD_ARCH% %MEM_OPT% toolset=%TOOLSET% install
 
 @endlocal
