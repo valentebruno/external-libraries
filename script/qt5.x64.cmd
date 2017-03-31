535a7b381b6d866778579c59a8faab423295b3c4
Walter Gray <wgray@leapmotion.com>
2017-03-31 16:40:12 -0700

    Fixes to the qt setup script

--- script/qt5.x64.cmd
+++ script/qt5.x64.cmd
@@ -1,28 +1,25 @@
 @setlocal
-@set root_dir=%CD%
 @call %VSSETUP_COMMAND%
-@set PATH=%CD%\src;%PATH%
-@cd src\%1
+@set PATH=%1\src;%PATH%
 @set PATH=ICU_BIN_PATH;%PATH%
 @set PATH=C:\Perl64\bin;%PATH%
 @set PATH=C:\Python27-x64;%PATH%
 @set PATH=C:\Ruby22-x64\bin;%PATH%
-@set PATH=%CD%\qtbase\bin;%CD%\gnuwin32\bin;%PATH%
-echo path=%PATH%
-rem Uncomment the below line when using a git checkout of the source repository
-SET PATH=%CD%\qtrepotools\bin;%PATH%
+@set PATH=%1\qtbase\bin;%1\gnuwin32\bin;%PATH%
+@rem Uncomment the below line when using a git checkout of the source repository
+@set PATH=%1\qtrepotools\bin;%PATH%
 @set QMAKESPEC=win32-msvc%MSVC_VER%
-@cd ..\..
+
 @mkdir build
 @cd build
 @mkdir qt
 @cd qt
-@call ..\..\src\%1\configure -no-avx2 -no-avx -no-sse4.2 -no-sse4.1 -c++11 -prefix "%2" -opensource ^
+@call ..\..\src\%1\configure -prefix "%2" -opensource ^
 -confirm-license -debug-and-release -opengl desktop -no-angle -no-incredibuild-xge ^
 -platform %QMAKESPEC% -mp -openssl-linked -nomake examples -nomake tests -no-icu -no-dbus ^
 -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d ^
 -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtdocgallery ^
--skip qtenginio -skip qtfeedback -skip qtgraphicaleffects -skip qtimageformats ^
+-skip qtenginio -skip qtfeedback -skip qtgamepad -skip qtgraphicaleffects -skip qtimageformats ^
 -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtpim -skip qtpurchasing ^
 -skip qtquick1 -skip qtquickcontrols -skip qtquickcontrols2 -skip qtrepotools ^
 -skip script -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtsvg ^
