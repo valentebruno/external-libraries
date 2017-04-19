9d91235113dd77454517beedea3437b918df2ff8
Walter Gray <wgray@leapmotion.com>
2017-04-18 18:58:35 -0700

    Bump to freeglut 3.0.0

--- linux/Build_flatbuffers.sh
+++ linux/Build_flatbuffers.sh
@@ -6,5 +6,5 @@ src_dir=$1
 ins_dir=$2
 cd src/${src_dir}
 
-CXXFLAGS="-fPIC" cmake . -DCMAKE_INSTALL_PREFIX=${ins_dir}
+CXXFLAGS="-fPIC" cmake . -DCMAKE_INSTALL_PREFIX=${ins_dir} -DHAVE_X11_EXTENSIONS_XF86VMODE_H:BOOL=OFF -DX11_xf86vmode_FOUND:BOOL=OFF
 make -j 9 && make install
