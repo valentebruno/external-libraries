e6aea5a780f245fe1905b2fc28caee9cf267afde
Walter Gray <chrysalisx@gmail.com>
2017-04-26 11:03:01 -0700

    Use https to allow anonymous checkouts

--- setup-all-libraries-win.sh
+++ setup-all-libraries-win.sh
@@ -7,8 +7,8 @@ setup-library https://chromium.googlesource.com/external/gyp 0.1 -g -b "master"
 
 source ./setup-all-libraries.sh
 
-setup-library git@github.com:dcnieho/FreeGLUT.git 3.0.0 -b FG_3_0_0
-setup-library git@github.com:leapmotion/DShowBaseClasses.git 1.0.0 -o "baseclasses-1.0.0"
+setup-library https://github.com/dcnieho/FreeGLUT.git 3.0.0 -g -b FG_3_0_0
+setup-library https://github.com/leapmotion/DShowBaseClasses.git 1.0.0 -g -o "baseclasses-1.0.0"
 
 SWIG_VERSION=3.0.3
 setup-library http://prdownloads.sourceforge.net/swig/swigwin-${SWIG_VERSION}.zip ${SWIG_VERSION} -s "swigwin-${SWIG_VERSION}" -o "swigwin-${SWIG_VERSION}" -n "swig"
