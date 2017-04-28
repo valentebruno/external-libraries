3b58c3bec1bd998e704ee34c050ccb2a154b38b7
Walter Gray <wgray@leapmotion.com>
2017-04-27 18:19:25 -0700

    Fix openssl build command.

--- arm-linux/Build_openssl.sh
+++ arm-linux/Build_openssl.sh
@@ -6,11 +6,12 @@ ins_dir=$2
 cd src/${src_dir}
 
 if [ "${BUILD_ARCH}" == "x64" ]; then
-  OPENSSL_OS="linux-generic64"
+  OPENSSL_OS="linux-aarch64"
 else
   OPENSSL_OS="linux-generic32"
 fi
 
+CC=gcc-${CROSS_COMPILER_VERSION}
 ./Configure -fPIC --prefix="${ins_dir}" ${OPENSSL_OS} no-asm enable-static-engine
 
 make -k -j 8 || true
