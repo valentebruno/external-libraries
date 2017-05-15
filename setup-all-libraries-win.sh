04318679add9002206db54c36ca7794e397a0414
James Donald <jdonald@leapmotion.com>
2017-05-15 15:10:35 -0700

    Local build settings for various VS2015 libs

--- setup-all-libraries-win.sh
+++ setup-all-libraries-win.sh
@@ -2,13 +2,13 @@
 
 source ./setup-library.sh
 
-setup-library http://download.icu-project.org/files/icu4c/57.1/icu4c-57_1-src.zip 57.1 -s "icu" -o "icu-57.1" -n "icu"
+#setup-library http://download.icu-project.org/files/icu4c/57.1/icu4c-57_1-src.zip 57.1 -s "icu" -o "icu-57.1" -n "icu"
 setup-library https://chromium.googlesource.com/external/gyp 0.1 -g -b "master"
 
 source ./setup-all-libraries.sh
 
-setup-library https://github.com/dcnieho/FreeGLUT.git 3.0.0 -g -b FG_3_0_0
-setup-library https://github.com/leapmotion/DShowBaseClasses.git 1.0.0 -g -o "baseclasses-1.0.0"
+#setup-library https://github.com/dcnieho/FreeGLUT.git 3.0.0 -g -b FG_3_0_0
+#setup-library https://github.com/leapmotion/DShowBaseClasses.git 1.0.0 -g -o "baseclasses-1.0.0"
 
 SWIG_VERSION=3.0.3
-setup-library http://prdownloads.sourceforge.net/swig/swigwin-${SWIG_VERSION}.zip ${SWIG_VERSION} -s "swigwin-${SWIG_VERSION}" -o "swigwin-${SWIG_VERSION}" -n "swig"
+#setup-library http://prdownloads.sourceforge.net/swig/swigwin-${SWIG_VERSION}.zip ${SWIG_VERSION} -s "swigwin-${SWIG_VERSION}" -o "swigwin-${SWIG_VERSION}" -n "swig"
--- setup-all-libraries.sh
+++ setup-all-libraries.sh
@@ -8,11 +8,11 @@ fi
 source ./setup-library.sh
 setup-library http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz 1.63.0 -s "boost_1_63_0" -o "boost_1_63_0" -n "boost"
 setup-library https://github.com/madler/zlib.git 1.2.8 -g
-setup-library https://github.com/openssl/openssl.git 1.0.2k -g -b "OpenSSL_1_0_2k"
+#setup-library https://github.com/openssl/openssl.git 1.0.2k -g -b "OpenSSL_1_0_2k"
 export OPENSSL_ROOT_DIR=${OPENSSL_PATH}
 
-setup-library https://curl.haxx.se/download/curl-7.53.1.tar.gz 7.53.1 -n curl -o "curl-7.53.1" -s "curl-7.53.1"
-setup-library git://code.qt.io/qt/qt5.git 5.8.0 -o "qt-5.8.0"
+#setup-library https://curl.haxx.se/download/curl-7.53.1.tar.gz 7.53.1 -n curl -o "curl-7.53.1" -s "curl-7.53.1"
+#setup-library git://code.qt.io/qt/qt5.git 5.8.0 -o "qt-5.8.0"
 setup-library https://github.com/google/flatbuffers.git 1.0.3 -g -b "@e97f38e" #Not actually 1.0.3
 setup-library https://github.com/google/protobuf.git 3.0.2 -g
 
@@ -25,23 +25,23 @@ setup-library https://github.com/leapmotion/leapserial.git 0.4.0 -g -o LeapSeria
 setup-library https://github.com/leapmotion/leaphttp.git 0.1.1 -g -o LeapHTTP-0.1.1$suffix
 setup-library https://github.com/leapmotion/leapipc.git 0.1.4 -g -o LeapIPC-0.1.4$suffix
 setup-library https://github.com/leapmotion/leapresource.git 0.1.0 -g -o LeapResource-0.1.0$suffix
-setup-library https://github.com/leapmotion/libxs.git 1.2.0 -g 
-setup-library https://github.com/zaphoyd/websocketpp.git 0.8.0 -g -b "develop" #technically 0.8.0-dev
+#setup-library https://github.com/leapmotion/libxs.git 1.2.0 -g 
+#setup-library https://github.com/zaphoyd/websocketpp.git 0.8.0 -g -b "develop" #technically 0.8.0-dev
 
-setup-library https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_29_1_RTM/src/nss-3.29.1-with-nspr-4.13.1.tar.gz 3.29.1 -s "nss-3.29.1" -o "nss-3.29.1" -n "nss"
-setup-library https://github.com/leapmotion/SDL2 2.0.4 -g -b static-library -n "sdl2"
-setup-library https://sourceforge.net/projects/glew/files/glew/1.13.0/glew-1.13.0.tgz 1.13.0 -s "glew-1.13.0" -o "glew-1.13.0" -n "glew"
+#setup-library https://ftp.mozilla.org/pub/security/nss/releases/NSS_3_29_1_RTM/src/nss-3.29.1-with-nspr-4.13.1.tar.gz 3.29.1 -s "nss-3.29.1" -o "nss-3.29.1" -n "nss"
+#setup-library https://github.com/leapmotion/SDL2 2.0.4 -g -b static-library -n "sdl2"
+#setup-library https://sourceforge.net/projects/glew/files/glew/1.13.0/glew-1.13.0.tgz 1.13.0 -s "glew-1.13.0" -o "glew-1.13.0" -n "glew"
 setup-library https://github.com/leapmotion/FreeImage.git 3.16.0 -g -n "freeimage" #3.17 is incompatible with gcc 4.8 due to a bug in arm_neon. see https://git.busybox.net/buildroot/commit/?id=326a681f1ee23b40db2637dc8dfe3e49cd58cd80
-setup-library https://github.com/leapmotion/anttweakbar.git 1.16 -g -b "develop"
-setup-library https://github.com/bulletphysics/bullet3.git 2.84 -g -b 2.84 -o "bullet-2.84"
-setup-library git://git.sv.nongnu.org/freetype/freetype2.git 2.6.3 -b VER-2-6-3 -o "freetype-2.6.3"
-setup-library https://github.com/rougier/freetype-gl.git 0.0.1 -g -b @f2edab9 -n freetypegl
-setup-library https://github.com/assimp/assimp.git 3.2 -g
+#setup-library https://github.com/leapmotion/anttweakbar.git 1.16 -g -b "develop"
+#setup-library https://github.com/bulletphysics/bullet3.git 2.84 -g -b 2.84 -o "bullet-2.84"
+#setup-library git://git.sv.nongnu.org/freetype/freetype2.git 2.6.3 -b VER-2-6-3 -o "freetype-2.6.3"
+#setup-library https://github.com/rougier/freetype-gl.git 0.0.1 -g -b @f2edab9 -n freetypegl
+#setup-library https://github.com/assimp/assimp.git 3.2 -g
 
 setup-library https://chromium.googlesource.com/breakpad/breakpad 0.1 -g -b "chrome_53"
 
 EIGEN_VERSION=3.3.1
-setup-library http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz ${EIGEN_VERSION} -o "eigen-${EIGEN_VERSION}" -s "eigen-eigen-f562a193118d" -n eigen
-setup-library https://github.com/memononen/nanosvg.git 1.0.0 -g -b "master"
-setup-library https://github.com/opencv/opencv.git 3.2.0 -g -b "3.2.0"
-setup-library https://github.com/ivanfratric/polypartition.git 1.0.0 -g -b "master"
+#setup-library http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz ${EIGEN_VERSION} -o "eigen-${EIGEN_VERSION}" -s "eigen-eigen-f562a193118d" -n eigen
+#setup-library https://github.com/memononen/nanosvg.git 1.0.0 -g -b "master"
+#setup-library https://github.com/opencv/opencv.git 3.2.0 -g -b "3.2.0"
+#setup-library https://github.com/ivanfratric/polypartition.git 1.0.0 -g -b "master"
