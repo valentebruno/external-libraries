e50dcb3abda6c21fb1389790b78b3596846e5c28
Walter Gray <wgray@leapmotion.com>
2017-05-19 15:18:44 -0700

    only apply project-config sed command if the option is given for this platform.

--- posix/Build_boost.sh
+++ posix/Build_boost.sh
@@ -10,5 +10,7 @@ rm -fr /tmp/$USER/boost
 ./bootstrap.sh ${boost_toolset}
 mkdir -p ${ins_dir}
 
-sed -i "${boost_compiler_patch}" project-config.jam
+if [[ -n ${boost_compiler_patch} ]]; then
+  sed -i "${boost_compiler_patch}" project-config.jam
+fi
 ./b2 --prefix="${ins_dir}" --build-dir=/tmp/$USER/ --with-atomic --with-chrono --with-date_time --with-filesystem --with-locale --with-program_options --with-thread --with-regex link=static threading=multi variant=release ${boost_additional_args} install
