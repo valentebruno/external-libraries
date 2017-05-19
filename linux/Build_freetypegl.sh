4992ec2b58dbd061ed0ac328c22da5760b7f6c3c
Walter Gray <wgray@leapmotion.com>
2017-05-19 01:22:48 +0000

    Fix linux freetypegl glew include paths

--- linux/Build_freetypegl.sh
+++ linux/Build_freetypegl.sh
@@ -2,7 +2,7 @@
 # Freetype-gl
 # ===========
 
-CMAKE_ADDITIONAL_ARGS="-DGLEW_INCLUDE_PATH=\"${GLEW_PATH}/include\" \
--DGLEW_LIBRARY=\"${GLEW_PATH}/lib64/libGLEW.a\""
+CMAKE_ADDITIONAL_ARGS="-DGLEW_INCLUDE_PATH:PATH=${GLEW_PATH}/include \
+-DGLEW_LIBRARY:FILEPATH=${GLEW_PATH}/lib64/libGLEW.a"
 
 source posix/$(basename $0)
