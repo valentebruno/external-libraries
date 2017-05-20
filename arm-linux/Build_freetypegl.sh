6dd5f130259dbefc9747ad9e7b76a516ae34e192
Walter Gray <wgray@leapmotion.com>
2017-05-20 01:49:53 +0000

    Fix arm-linux freetypegl

--- arm-linux/Build_freetypegl.sh
+++ arm-linux/Build_freetypegl.sh
@@ -2,6 +2,6 @@
 # Freetype-gl
 # ===========
 
-export CMAKE_ADDITIONAL_ARGS="${CMAKE_ADDITIONAL_ARGS} -DGLEW_INCLUDE_PATH=\"${GLEW_PATH}/include\" -DGLEW_LIBRARY=\"${GLEW_PATH}/lib64/libGLEW.a\""
+export CMAKE_ADDITIONAL_ARGS="${CMAKE_ADDITIONAL_ARGS} -DGLEW_INCLUDE_PATH:PATH=${GLEW_PATH}/include -DGLEW_LIBRARY:PATH=${GLEW_PATH}/lib64/libGLEW.a"
 
 source posix/$(basename $0)
