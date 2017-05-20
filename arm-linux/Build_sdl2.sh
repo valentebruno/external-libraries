57dcde090427d0b548ce411c30d565d42edfe010
Walter Gray <wgray@leapmotion.com>
2017-05-20 01:50:51 +0000

    Fix arm-linux sdl2

--- arm-linux/Build_sdl2.sh
+++ arm-linux/Build_sdl2.sh
@@ -3,8 +3,5 @@
 # ====
 
 export cfg_args="--host=arm-linux"
-./configure --prefix="${ins_dir}" --disable-shared --enable-static \
---disable-audio --disable-joystick --disable-haptic -host=arm-linux
-make -j 4
-make install
-cd ..
+source posix/$(basename $0)
+
