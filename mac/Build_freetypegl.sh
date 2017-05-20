6cecbee8e876608f28e48600a05385ed7696f6f2
Walter Gray <wgray@leapmotion.com>
2017-05-19 19:30:31 -0700

    Fix freetype-gl mac

--- mac/Build_freetypegl.sh
+++ mac/Build_freetypegl.sh
@@ -2,13 +2,5 @@
 # Freetype-gl
 # ===========
 
-src_dir=$1
-ins_dir=$2
-cd src/${src_dir}
-
-mkdir build
-cd build
-
 export CMAKE_ADDITIONAL_ARGS="-Dfreetype-gl_BUILD_TESTS=OFF"
-
 source posix/$(basename $0)
