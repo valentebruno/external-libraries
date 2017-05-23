1d57abaed4645a3a57e0340354c9cca4f8465e12
Walter Gray <wgray@leapmotion.com>
2017-05-23 22:10:53 +0000

    Use a build directory for autowiring

--- posix/Build_autowiring.sh
+++ posix/Build_autowiring.sh
@@ -6,7 +6,10 @@ src_dir=$1
 ins_dir=$2
 cd src/${src_dir}
 
-cmake . -DCMAKE_INSTALL_PREFIX:PATH=${ins_dir} ${CMAKE_ADDITIONAL_ARGS}
+mkdir -p build
+cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX:PATH=${ins_dir} ${CMAKE_ADDITIONAL_ARGS}
 
 cmake --build . --target install --config Debug -- -j8
 cmake --build . --target install --config Release -- -j8
