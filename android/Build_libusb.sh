9705d34eccf0e64be9c31c8a6a8d8d00e9298dd1
Walter Gray <wgray@leapmotion.com>
2017-05-24 01:21:15 +0000

    Fix android libusb

--- android/Build_libusb.sh
+++ android/Build_libusb.sh
@@ -2,5 +2,6 @@
 # libusb
 # ======
 
+export cfg_flags="--host=${HOST}"
 source posix/$(basename $0)
 cp libusb/.libs/libusb-1.0.so ${ins_dir}/lib/libusb-1.0.0.so
--- setup-all-libraries-android64.sh
+++ setup-all-libraries-android64.sh
@@ -23,6 +23,7 @@ export SKIP_SWIG_BUILD=true
 export CMAKE_ADDITIONAL_ARGS="-DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}"
 
 #override the libusb branch
+source setup-library.sh
 setup-library https://github.com/leapmotion/libusb.git 1.0.0 -g -b leap-2.3.x-arm
 
 source setup-all-libraries-posix.sh
