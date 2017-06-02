5b7049cd7c4112af3ca6776fafd1e3b36b151d30
Walter Gray <wgray@leapmotion.com>
2017-06-02 15:10:48 -0700

    Add debug suffix for freetypegl builds

--- win/freetypegl.sh
+++ win/freetypegl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-CMAKE_ADDITIONAL_ARGS="-DFREETYPE_LIBRARY=${FREETYPE2_PATH}/lib/libfreetype.lib"
+CMAKE_ADDITIONAL_ARGS="-DFREETYPE_LIBRARY=${FREETYPE2_PATH}/lib/libfreetype.lib -DCMAKE_DEBUG_POSTFIX=d"
 
 freetype_lib_dir="Debug"
 source posix/$(basename $0)
