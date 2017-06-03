bd8ab2f87468434e639b44b15d98a718393b717b
Walter Gray <wgray@leapmotion.com>
2017-06-02 17:05:07 -0700

    switch to cmake for glew on windows to fix linking with /MT

--- win/glew.sh
+++ win/glew.sh
@@ -4,13 +4,6 @@ src_dir=$1
 ins_dir=$2
 cd src/${src_dir}
 
-cd build/vc12
-devenv //Upgrade glew.sln
-msbuild.exe glew_static.vcxproj //p:Configuration=Debug;Platform=${BUILD_ARCH}
-msbuild.exe glew_static.vcxproj //p:Configuration=Release;Platform=${BUILD_ARCH}
-cd ../..
+cd build/cmake
 
-mkdir -p ${ins_dir}/lib
-cp lib/*/*/* ${ins_dir}/lib/
-cp -r include ${ins_dir}
-cp -r doc ${ins_dir}
+build_cmake_lib "${ins_dir}" -DBUILD_UTILS:BOOL=FALSE -DBUILD_SHARED_LIBS:BOOL=FALSE
