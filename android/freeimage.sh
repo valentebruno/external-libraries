c0702474b5c6fcdc725edb615826b129218342e2
Walter Gray <wgray@leapmotion.com>
2017-06-08 19:55:21 +0000

    Fix missing header in FreeImage for android32

--- android/freeimage.sh
+++ android/freeimage.sh
@@ -3,4 +3,7 @@
 # =========
 
 export CFLAGS="${CFLAGS} -DPNG_ARM_NEON_OPT=0"
+if [[ $BUILD_ARCH == x86 ]]; then
+  export CFLAGS="${CFLAGS} -I${NDK_ROOT}/sources/android/cpufeatures -include cpu-features.h"
+fi
 source posix/$(basename $0)
