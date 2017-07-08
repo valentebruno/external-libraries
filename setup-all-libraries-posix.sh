5661f582b06f242421c1d7b2f69de10e8ad7e821
James Donald <jdonald@leapmotion.com>
2017-07-07 17:53:54 -0700

    My local changes to build only leapipc

--- setup-all-libraries-posix.sh
+++ setup-all-libraries-posix.sh
@@ -11,15 +11,15 @@ export CMAKE_BUILD_ARGS="-j4"
 source setup-library.sh
 
 #required for bullet
-setup-library http://prdownloads.sourceforge.net/freeglut/freeglut-3.0.0.tar.gz 3.0.0 -s "freeglut-3.0.0" -o "freeglut-3.0.0" -n "freeglut"
+#setup-library http://prdownloads.sourceforge.net/freeglut/freeglut-3.0.0.tar.gz 3.0.0 -s "freeglut-3.0.0" -o "freeglut-3.0.0" -n "freeglut"
 
 source setup-all-libraries.sh
 
 BZIP2_VERSION="1.0.6"
-setup-library http://www.bzip.org/${BZIP2_VERSION}/bzip2-${BZIP2_VERSION}.tar.gz ${BZIP2_VERSION} -n "bzip2" -s "bzip2-${BZIP2_VERSION}" -o "bzip2-${BZIP2_VERSION}"
+#setup-library http://www.bzip.org/${BZIP2_VERSION}/bzip2-${BZIP2_VERSION}.tar.gz ${BZIP2_VERSION} -n "bzip2" -s "bzip2-${BZIP2_VERSION}" -o "bzip2-${BZIP2_VERSION}"
 
-setup-library https://github.com/leapmotion/libusb.git 1.0.0 -g -b leap-2.2.x
-setup-library http://sf-github.leap.corp/leapmotion/libuvc.git 1.0.0 -g -b "master"
+#setup-library https://github.com/leapmotion/libusb.git 1.0.0 -g -b leap-2.2.x
+#setup-library http://sf-github.leap.corp/leapmotion/libuvc.git 1.0.0 -g -b "master"
 
 SWIG_VERSION=3.0.3
-setup-library http://iweb.dl.sourceforge.net/project/swig/swig/swig-${SWIG_VERSION}/swig-${SWIG_VERSION}.tar.gz ${SWIG_VERSION} -s "swig-${SWIG_VERSION}" -o "swig-${SWIG_VERSION}" -n "swig"
+#setup-library http://iweb.dl.sourceforge.net/project/swig/swig/swig-${SWIG_VERSION}/swig-${SWIG_VERSION}.tar.gz ${SWIG_VERSION} -s "swig-${SWIG_VERSION}" -o "swig-${SWIG_VERSION}" -n "swig"
