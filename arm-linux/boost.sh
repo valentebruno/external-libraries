2808e221d5c47181e7c030b511efcc08acba0ca4
Raimo Tuisku <raimo.tuisku@gmail.com>
2017-08-07 22:04:11 -0700

    Compile using gcc, not clang, for Android.

--- arm-linux/boost.sh
+++ arm-linux/boost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-# Boost 1.63
-# ===================
+# Boost
+# =====
 
 boost_compiler_patch="s/using gcc ;/using gcc : arm : $(basename ${CXX}) ;/"
 boost_additional_args=toolset="gcc-arm target-os=linux"
