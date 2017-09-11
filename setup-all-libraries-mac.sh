b110f5838db93e384f3c806eae56e5d368c8b14c
Raimo Tuisku <raimo.tuisku@gmail.com>
2017-09-11 13:24:08 -0700

    Also compile 32bit for build servers.

--- setup-all-libraries-mac.sh
+++ setup-all-libraries-mac.sh
@@ -9,7 +9,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.10
 export CMAKE_ADDITIONAL_ARGS="-DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.10 -DCMAKE_OSX_SYSROOT=macosx10.12"
 export CC=clang
 export CXX=clang++
-export CFLAGS="-mmacosx-version-min=10.10 -arch x86_64"
+export CFLAGS="-mmacosx-version-min=10.10 -arch i386 -arch x86_64"
 export CXXFLAGS="-stdlib=libc++"
 
 source setup-library.sh
