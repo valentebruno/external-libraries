0ffb7f08ad996b555ef07e7bf7c017daf4d1fc07
Raimo Tuisku <raimo.tuisku@gmail.com>
2017-09-14 14:01:41 -0700

    Resolve symlinks.

--- setup-library.sh
+++ setup-library.sh
@@ -24,7 +24,7 @@ function brew_install {
   [[ -d $BREW_PATH ]] || git clone https://github.com/Linuxbrew/brew $BREW_PATH
   [[ -d $output_dir ]] && echo "$formula already installed." && return
 
-  $BREW_PATH/bin/brew install $formula && cp -r $BREW_PATH/Cellar/$formula/$version/ $output_dir
+  $BREW_PATH/bin/brew install $formula && cp -rL $BREW_PATH/Cellar/$formula/$version/ $output_dir
 }
 
 function pip3_install {
