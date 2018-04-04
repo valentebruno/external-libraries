b942fe8ce4d6522e28d7dbbdbd0ee3a5bc528378
James Donald <jdonald@leapmotion.com>
2018-04-04 15:36:09 -0700

    Restore arm-linux/qt5.sh using linux-rasp-pi3-g++

--- arm-linux/qt5.sh
+++ arm-linux/qt5.sh
@@ -2,18 +2,32 @@
 # Qt5
 # ============
 
-export QT_SRC_DIR=${BUILD_DIR}/$1
-QT_DEVICE=linux-leap-lmvp-g++
-QT_QPA_PLATFORM=xcb
+# TODO: Support 64-bit ARM Linux e.g. Tegra TX2
+# FIXME: When posix/qt5.sh adds openssl support, fix here as well
 
-cp -vr arm-linux/mkspecs/* ${QT_SRC_DIR}/qtbase/mkspecs/
+src_dir=$1
+ins_dir=$2
+cd ${BUILD_DIR}/${src_dir}
 
-#-no-feature-pdf added because the module is broken in GCC 4.8.
-export QMAKE_CC=${CC}
-export QMAKE_CXX=${CXX}
-export OPENSSL_LIBS="-L${OPENSSL_PATH}/lib -lssl -lcrypto -ldl"
-export qt_additional_args="-device ${QT_DEVICE} \
--device-option CROSS_COMPILE=${CROSS_COMPILER_PREFIX} -device-option COMPILE_VERSION=5 \
--openssl-linked -I${OPENSSL_PATH}/include -qt-libjpeg -qt-libpng -qt-xcb -no-feature-pdf"
+export QT_QPA_PLATFORM=xcb
+./configure \
+ -platform linux-g++-64 \
+ -device linux-rasp-pi3-g++ \
+ -device-option CROSS_COMPILE=${CROSS_COMPILER_PREFIX} \
+ -device-option COMPILE_VERSION=6 \
+ -prefix "${ins_dir}" -opensource -confirm-license -release \
+ -no-pch -no-icu -nomake examples -nomake tests \
+ -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d \
+ -skip qtconnectivity -skip qtdeclarative -skip qtdoc -skip qtdocgallery \
+ -skip qtenginio -skip qtfeedback -skip qtgamepad -skip qtgraphicaleffects \
+ -skip qtimageformats -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtpim \
+ -skip qtpurchasing -skip qtquick1 -skip qtquickcontrols -skip qtquickcontrols2 \
+ -skip qtrepotools -skip script -skip qtsensors -skip qtserialbus -skip qtserialport \
+ -skip qtsvg -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebkit \
+ -skip qtwebkit-examples -skip qtx11extras -skip qtxmlpatterns \
+ ${qt_additional_args} --verbose \
+ && make_check_err -j 9 VERBOSE=1 &&
+make_check_err -j1 install
 
-source posix/$(basename $0)
+rm -fr ${ins_dir}/doc
+rm -fr ${ins_dir}/examples
