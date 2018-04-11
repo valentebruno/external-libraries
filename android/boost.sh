dfc3a0bb2fe8dce2f4cb775abca020e2aedccd0f
Walter Gray <wgray@leapmotion.com>
2018-04-11 13:42:37 -0700

    fix boost

--- android/boost.sh
+++ android/boost.sh
@@ -2,6 +2,19 @@
 # Boost 1.63
 # ===================
 
-export boost_compiler_patch="s/using [^ ]* ;/using gcc : arm : $(basename ${CXX}) ;/"
-export boost_additional_args="toolset=gcc-arm target-os=linux"
+user_config=~/user-config.jam
+echo "Generating config in $user_config..."
+rm -f $user_config
+cat <<EOF > $user_config
+import os ;
+using clang : android :
+	${CXX} :
+	<archiver>${CROSS_COMPILER_PATH}/${CROSS_COMPILER_PREFIX}ar
+	<ranlib>${CROSS_COMPILER_PATH}/${CROSS_COMPILER_PREFIX}ranlib
+	<compileflags>--sysroot=${SYSROOT}
+	<compileflags>${CFLAGS}
+;
+EOF
+
+export boost_additional_args="toolset=clang-android target-os=android"
 source posix/$(basename $0)
