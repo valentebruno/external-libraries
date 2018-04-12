9a263e6a701ba8ee68b2cdbfcccc1e0a44335968
Walter Gray <wgray@leapmotion.com>
2018-04-11 18:45:15 -0700

    fix missing include in libusb

--- android/libusb.sh
+++ android/libusb.sh
@@ -3,5 +3,6 @@
 # ======
 
 export cfg_flags="--host=${HOST}"
+export CFLAGS="${CFLAGS} --include=sys/sysmacros.h"
 source posix/$(basename $0)
 cp libusb/.libs/libusb-1.0.so ${ins_dir}/lib/libusb-$3.so
