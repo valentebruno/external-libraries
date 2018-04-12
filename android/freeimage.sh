761b81240ebd1cf229dde899ec13119b1026d1a3
Walter Gray <wgray@leapmotion.com>
2018-04-12 15:11:43 -0700

    fix redundant -include directive

--- android/freeimage.sh
+++ android/freeimage.sh
@@ -4,6 +4,6 @@
 
 export CFLAGS="${CFLAGS} -DPNG_ARM_NEON_OPT=0"
 if [[ $BUILD_ARCH == x86 ]]; then
-  export CFLAGS="${CFLAGS} -I${NDK_ROOT}/sources/android/cpufeatures -include cpu-features.h"
+  export CFLAGS="${CFLAGS} -I${NDK_ROOT}/sources/android/cpufeatures"
 fi
 source posix/$(basename $0)
